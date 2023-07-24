dc9478aeb94317a357ce14c8c7c20bf71f4f2413
Muskan <muskankumarix381@gmail.com>
2023-07-24 11:24:37 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+// import ReactDOM from 'react-dom';
+import App from './App';
+
+
+//  ReactDOM.render(<App />,document.getElementById("root"));
+
+
+
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
